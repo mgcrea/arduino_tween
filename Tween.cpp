44bc2bc2802a54914aeaa36d5a3fc164c124fb5f
Olivier Louvignes <olivier@mg-crea.com>
2015-05-28 18:23:21 +0200

    refactor(arduino): refactor project to compile on arduino
    Breaking: TweenTimer::tick() is now based on millis().

--- Tween.cpp
+++ Tween.cpp
@@ -12,27 +12,31 @@ Tween::Tween()
 /*	Setup methods
  ___________________________________________________ */
 
-void Tween::setup(int duration, float start, float change, float (* ease)(float, float, float, float), int delay, int loop)
+void Tween::setup(int duration, float begin, float finish, float (* ease)(float, float, float, float), int delay, int loop)
 {
+	_tween.reset();
 	_tween.duration = duration;
 	_tween.loop = loop;
+	_delay.reset();
 	_delay.duration = delay;
-	
-	_start = start;
-	_change = change;
-	
-	num = start;
+
+	_begin = begin;
+	_finish = finish;
+	_change = finish - begin;
+
+	num = begin;
+	percent = 0;
 	_ease = ease;
 }
 
-void Tween::setup(int duration, float start, float change, float (* ease)(float, float, float, float))
+void Tween::setup(int duration, float begin, float change, float (* ease)(float, float, float, float))
 {
-	setup(duration, start, change, ease, 0, 0);
+	setup(duration, begin, change, ease, 0, 0);
 }
 
-void Tween::setup(int duration, float start, float change, float (* ease)(float, float, float, float), int delay)
+void Tween::setup(int duration, float begin, float change, float (* ease)(float, float, float, float), int delay)
 {
-	setup(duration, start, change, ease, delay, 0);
+	setup(duration, begin, change, ease, delay, 0);
 }
 
 /*	Update
@@ -40,26 +44,55 @@ void Tween::setup(int duration, float start, float change, float (* ease)(float,
 
 void Tween::update()
 {
-	if(_delay.duration > 0)
-	{
-		_delay.tick();
-		
-		if(_delay.time == _delay.duration)
-		{
-			_tween.state = 1;
+
+	if(_delay.duration > 0) {
+		if(_delay.time == _delay.duration && _delay.state == 0) {
+			Serial.println("delay finished");
+			// _tween.created = millis();
+			// _tween.state = _tween.count % 2 == 0 ? 1 : -1;
 		}
+		_delay.tick();
 	}
-	
+
 	_tween.tick();
-	
+
 	percent = _tween.time / _tween.duration;
+
+	if(_tween.time == _tween.duration) {
+		// Serial.println("DOWN!");
+		// Serial.println(_tween.count);
+		_tween.state = -1;
+	} else if(_tween.state == 1 && _tween.time == 0) {
+		// Serial.println("UP!");
+		// Serial.println(_tween.count);
+		// _delay.time = 0;
+		// _delay.created = millis();
+		// _tween.state = 0;
+	}
 
 	runEasing();
 }
 
 void Tween::runEasing()
 {
-	num = _ease(_tween.time, _start, _change, _tween.duration);
+	num = _ease(_tween.time, _begin, _change, _tween.duration);
+
+	// Debug
+	Serial.print("num=");
+	Serial.print(num);
+	Serial.print(",tween_state=");
+	Serial.print(_tween.state);
+	Serial.print(",tween_time=");
+	Serial.print(static_cast<int>(_tween.time));
+	Serial.print("/");
+	Serial.print(static_cast<int>(_tween.duration));
+	Serial.print(",delay_state=");
+	Serial.print(_delay.state);
+	Serial.print(",delay_time=");
+	Serial.print(static_cast<int>(_delay.time));
+	Serial.print("/");
+	Serial.println(static_cast<int>(_delay.duration));
+
 }
 
 /*	Control methods
@@ -67,11 +100,14 @@ void Tween::runEasing()
 
 void Tween::play()
 {
+	_tween.created = millis();
+	_delay.created = millis();
+
 	if(_delay.duration > 0 && _delay.time != _delay.duration)
 	{
 		_delay.state = 1;
 	}
-	else 
+	else
 	{
 		_tween.state = 1;
 	}
@@ -86,11 +122,15 @@ void Tween::pause()
 void Tween::stop()
 {
 	pause();
-	
+
 	_delay.time = 0;
 	_tween.time = 0;
 }
 
+bool Tween::isActive()
+{
+	return _delay.state != 0 || _tween.state != 0 ? true : false;
+}
 bool Tween::finished()
 {
 	return _tween.time == _tween.duration ? true : false;
--- Tween.h
+++ Tween.h
@@ -1,49 +1,68 @@
 #pragma once
 
+#include <Arduino.h>
 #include "Easing.h"
 
 /*	TweenTimer
  ___________________________________________________ */
 
-class TweenTimer 
+class TweenTimer
 {
 public:
-	
-	int state; 
-	float time, duration;
-	int loop;
-	
-	TweenTimer() 
+
+	int state;
+	float created, time, duration;
+	unsigned int loop, count;
+
+	TweenTimer()
 	{
+		reset();
+	}
+
+	void reset() {
+		created = millis();
+		count = 0;
 		loop = 0;
-		time = 0.0; 
-		duration = 0.0; 
+		time = 0.0;
+		duration = 0.0;
 		state = 0;
 	}
-	
-	void tick()
-	{
-		time += state;
-		
-		if (time > duration) 
-		{
-			time = duration;
-			
-			if(loop == 0)
-				state = 0;
-			else if(loop == 1)
-				state = time = 0;
-			else if(loop == 2)
-				state -1;
+
+	void tick() {
+
+		int ctime = millis();
+
+		if(state == 0) {
+			return;
+		} else if(state == 1) {
+			time = ctime - created;
+		} else if(state == -1) {
+			time = (created + duration) - ctime;
 		}
-		else if(time < 0)
-		{
-			time = 0;
-			
-			if(loop == 0)
+
+		if (time > duration) {
+			count++;
+			time = duration;
+			created = ctime;
+
+			if(loop == 0) {
 				state = 0;
-			else if(loop == 2)
+			} else if(loop == 1) {
+				time = 0;
+        state = 1;
+			} else if(loop == 2) {
+				state = -1;
+			}
+		} else if(time < 0) {
+			count++;
+			time = 0;
+			created = ctime;
+
+			if(loop == 0) {
+        state = 0;
+      } else if(loop > 0) {
 				state = 1;
+			}
 		}
 	}
 };
@@ -52,9 +71,9 @@ public:
  ___________________________________________________ */
 
 class Tween {
-	
+
 public:
-	
+
 	Tween();
 	void setup(int duration, float start, float change, float (* ease)(float, float, float, float));
 	void setup(int duration, float start, float change, float (* ease)(float, float, float, float), int delay);
@@ -62,22 +81,24 @@ public:
 	void play();
 	void pause();
 	void stop();
-	
+
+	bool isActive();
 	bool finished();
-	
+
 	void update();
-	
+
 	float num;
 	float percent;
-	
+
 private:
-	
+
 	TweenTimer _tween;
 	TweenTimer _delay;
-	
-	float _start;
-	float _change;
+
+	float _begin;
+  float _finish;
+  float _change;
 	float (* _ease)(float, float, float, float);
-	
+
 	void runEasing();
 };
